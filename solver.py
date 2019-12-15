Acceleration sign fix | Chute __openTimeFix

solver.py
@@ -13,26 +13,30 @@ from ChuteManager import ChuteManager
 from environment import rho, g
 
 mass = 60
-chutes = ChuteManager()
+chutes = ChuteManager()  # klärt, welcher Fallschirm gerade genutzt wird
 
-chute = Chute()
+chute = Chute()  # aktuell einziger Fallschirm
 chute.A_max = 2.5
 chute.cutHeight = -1
-chute.openingHeight = 300
+chute.openingHeight = 1000
 chute.cw = 2
 chute.openingDelay = 0
-chute.openingDuration = 3
+chute.openingDuration = 5  # muss positiv ungleich 0 sein
 
 chutes.addChute(chute)
 
 args = (mass, chutes)
 
 
+# print("mass: ", args[0])
+# print("chutes: ", args[1].getName())
+
+
 def height(x, t, *args):
     pos_y, velocity_y = x
 
     if pos_y > 0:
-        a = acceleration(x, t, args)
+        a = acceleration(x, t, *args)
         f = [velocity_y, a]
     else:
         f = [0, 0]
@@ -41,18 +45,21 @@ def height(x, t, *args):
 
 def acceleration(x, t, *args):
     pos_y, velocity_y = x
-    # print("mass: ", args[0][0])
-    # print("chutes: ", args[0][1].getName())
-    acc_y = 0.5 * rho(pos_y) * velocity_y**2 * args[0][1].getCWA(t, pos_y) / args[0][0]
-    return - acc_y - g(pos_y)
+    sign = velocity_y / abs(velocity_y)
+    # print(args[1].getCWA(t, pos_y))
+    acc_y = - sign * 0.5 * rho(pos_y) * velocity_y ** 2 * args[1].getCWA(t, pos_y) / args[0]  # luftwiderstand
+    return acc_y - g(pos_y)  # gesamtbeschleunigung
 
 
-x0 = [2000, 0]  # [pos_y, vel_y] StartingVector
-t = np.linspace(0, 120, 1000)
+x0 = [2000, 80]  # [pos_y, vel_y] StartingVector
+t = np.linspace(0, 100, 10000)
 x = odeint(height, x0, t, args=args)
-print(x)
+chute.printDate()
+# print(x)  # print Lösungsvektor der Höhe (?)
 
 pl.figure(1)
 pl.plot(t, x[:, 0])
 pl.legend()
+pl.figure(2)
+pl.plot(t, x[:, 1])
 pl.show()