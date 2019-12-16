66972c6cfe49ebd9272dca828ee105e6b5629bbd
bastuck <bayram.st@web.de>
2019-12-16 12:51:30 +0100

    little main cleanup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
+
 from IO import Input, Output
-
-
-# InputArguments
 from solver.solver import solve
 
 mass = 60  # Masse der Rakete
@@ -14,7 +12,3 @@ args = (mass, chutes)  # additional arguments
 
 x = solve(x0, t, *args)  # [pos_x, pos_y, vel_x, vel_y, acc_x, acc_y]
 Output.visualize(x, t, (0, 1, 2, 3, 4, 5))
-
-print("finish")
-
-
