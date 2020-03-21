18856dca215d0337ea63b89e92239aa4a6296afc
Brian Wirth <brian.wirth@cognostics.de>
2020-03-21 21:22:15 +0100

    remove package lock

--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,13 @@
 from IO.Input import Input
 from solver.Solver import solve
 from IO.Output import Output
-import numpy
+
 
 def main():
     params = Input()
     params.retrieve_input()
     visualizer = Output()
     x = solve(params.x0, params.t, params.mass, params.chute_manager)
-    print(repr(x[:,0]))
     visualizer.visualize(x, params.t)
 
 
