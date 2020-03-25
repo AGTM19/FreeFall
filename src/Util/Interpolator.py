618c687e53772f6dcc3e802163f6c771d45fe9b5
AGTM19 <bayram.st@web.de>
2020-03-25 15:50:33 +0100

    Interpolater is a class now

--- src/Util/Interpolator.py
+++ src/Util/Interpolator.py
@@ -1,67 +1,67 @@
-from numpy import integer
+class Interpolator:
 
+    @staticmethod
+    def getInterpolatedValue(d, key):
+        """
 
-def getInterpolatedValue(d, key):
-    """
+        :param d: Dictionary mapping numbers to numbers
+        :param key: key within range of dictKeys
+        :return: interpolated Value of dict keys
+        """
 
-    :param d: Dictionary mapping numbers to numbers
-    :param key: key within range of dictKeys
-    :return: interpolated Value of dict keys
-    """
+        keys = Interpolator.__getKeyList__(d)
 
-    keys = __getKeyList__(d)
+        valid_keys = Interpolator.__getValidKey__(keys, key)
+        if valid_keys[0] == valid_keys[1]:
+            return d[key]
+        return Interpolator.__interpolate__(key, valid_keys, d)
 
-    valid_keys = __getValidKey__(keys, key)
-    if valid_keys[0] == valid_keys[1]:
-        return d[key]
-    return __interpolate__(key, valid_keys, d)
+    @staticmethod
+    def __getKeyList__(d):
+        keys = []
+        for k in d.keys():
+            keys.append(k)
+        keys.sort()
+        return keys
 
+    @staticmethod
+    def __interpolate__(x, xv, d):
+        y0 = d[xv[0]]
+        y1 = d[xv[1]]
+        return y0 + (y1 - y0) * (x - xv[0]) / (xv[1] - xv[0])
 
-def __getKeyList__(d):
-    keys = []
-    for k in d.keys():
-        keys.append(k)
-    keys.sort()
-    return keys
+    @staticmethod
+    def __getValidKey__(keys, key):
+        keys.sort()
+        key_max = max(keys)
+        key_min = min(keys)
+        if key <= key_min:
+            return key_min, key_min
+        if key >= key_max:
+            return key_max, key_max
 
+        key0 = 0
+        key1 = 0
 
-def __interpolate__(x, xv, d):
-    y0 = d[xv[0]]
-    y1 = d[xv[1]]
-    return y0 + (y1 - y0) * (x - xv[0]) / (xv[1] - xv[0])
+        for k in keys:
+            if key == k:
+                return key, key
 
+        i = 0
+        for k in keys:
+            if key < k:
+                key1 = keys[i]
+                break
+            i = i + 1
 
-def __getValidKey__(keys, key):
-    keys.sort()
-    key_max = max(keys)
-    key_min = min(keys)
-    if key <= key_min:
-        return key_min, key_min
-    if key >= key_max:
-        return key_max, key_max
+        i = len(keys) - 1
+        for k in reversed(keys):
+            if key > k:
+                key0 = keys[i]
+                break
+            i = i - 1
 
-    key0 = 0
-    key1 = 0
-
-    for k in keys:
-        if key == k:
-            return key, key
-
-    i = 0
-    for k in keys:
-        if key < k:
-            key1 = keys[i]
-            break
-        i = i + 1
-
-    i = len(keys) - 1
-    for k in reversed(keys):
-        if key > k:
-            key0 = keys[i]
-            break
-        i = i - 1
-
-    return key0, key1
+        return key0, key1
 
 
 def main():
@@ -76,9 +76,9 @@ def main():
     x = 0.76478
     print("")
     print("x:", 5 * "\t", x)
-    print("keys:", 4 * "\t", __getKeyList__(d))
-    print("valid Keys:", 2 * "\t", __getValidKey__(__getKeyList__(d), x))
-    print("interpolated value:\t", getInterpolatedValue(d, x))
+    print("keys:", 4 * "\t", Interpolator.__getKeyList__(d))
+    print("valid Keys:", 2 * "\t", Interpolator.__getValidKey__(Interpolator.__getKeyList__(d), x))
+    print("interpolated value:\t", Interpolator.getInterpolatedValue(d, x))
 
 
 # run demo
